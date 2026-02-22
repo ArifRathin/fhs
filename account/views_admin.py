ee15ce75b8beff1f233817e848e8ff7c46c2329b
Arif Rathin <arif.rathin@gmail.com>
2026-02-22 11:53:27 +0600

    quotation and url correction

--- account/views_admin.py
+++ account/views_admin.py
@@ -158,6 +158,8 @@ def deleteAdmin(request, adminId):
 
 
 def loginUser(request):
+    if request.user.is_authenticated:
+        return redirect('home')
     if request.method == 'POST':
         email = request.POST.get('email')
         password = request.POST.get('password')
--- fhs/views.py
+++ fhs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from fault_report.models import FaultReport
 from account.models import User
 from notification.models import Notification
@@ -6,21 +6,22 @@ from front_page.models import FrontPage, CustomerReview, Team
 from django.http import JsonResponse
 
 def home(request):
-    totalCompleted = FaultReport.objects.filter(completed_at__isnull=False).count()
-    uniqueCustomers = User.objects.filter(fault_report__isnull=True,is_enduser=True).count()
-    totalLocations = FaultReport.objects.distinct('location').count()
-    frontPage = FrontPage.objects.all().first()
-    customerReviews = CustomerReview.objects.filter(is_published=True).order_by('?')[:5]
-    members = Team.objects.filter(is_published=True).order_by('id')[:5]
-    data = {
-        'total_completed':totalCompleted,
-        'unique_customers':uniqueCustomers,
-        'total_locations':totalLocations,
-        'front_page':frontPage,
-        'customer_reviews':customerReviews,
-        'members':members
-    }
-    return render(request, 'base-front-end/enduser/home.html', data)
+    return redirect('login-user')
+    # totalCompleted = FaultReport.objects.filter(completed_at__isnull=False).count()
+    # uniqueCustomers = User.objects.filter(fault_report__isnull=True,is_enduser=True).count()
+    # totalLocations = FaultReport.objects.distinct('location').count()
+    # frontPage = FrontPage.objects.all().first()
+    # customerReviews = CustomerReview.objects.filter(is_published=True).order_by('?')[:5]
+    # members = Team.objects.filter(is_published=True).order_by('id')[:5]
+    # data = {
+    #     'total_completed':totalCompleted,
+    #     'unique_customers':uniqueCustomers,
+    #     'total_locations':totalLocations,
+    #     'front_page':frontPage,
+    #     'customer_reviews':customerReviews,
+    #     'members':members
+    # }
+    # return render(request, 'base-front-end/enduser/home.html', data)
 
 
 def updateNotifStatus(request):
